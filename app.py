f585f0ed3228ad065e4c907bd8b2818f9575930b
logan n <thegangreneforums@gmail.com>
2025-01-10 04:02:39 +1300

    shiny test

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print('test app')
+from shiny.express import input, render, ui
+
+ui.input_slider("val", "Slider label", min=0, max=100, value=50)
+
+@render.text
+def slider_val():
+    return f"Slider value: {input.val()}"
