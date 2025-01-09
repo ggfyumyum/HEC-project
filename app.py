185db03a3d17f9e66e4cca9a8127c45c8d7e1dd1
logan n <thegangreneforums@gmail.com>
2025-01-10 07:43:52 +1300

    tidying

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from shiny.express import input, render, ui
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from data_validation import Validator
+from data_analysis import Processor
+from data_vizualisation import Viz
+from eq5d_profile import eq5dvalue
+from eq5d_decrement_processing import decrement_processing
+
+from shiny import reactive
+from shiny.express import input, ui, render
 
 ui.input_slider("val", "Slider label", min=0, max=100, value=50)
+
+ui.input_text("text", label="Enter some text")
+
+data = ui.input_file(
+    'datafile',
+    'Upload xl file',
+    multiple=False,
+    accept=['.csv'],
+    button_label='Browse...',
+    placeholder='No file selected',
+    capture=None,
+)
+data
 
 @render.text
 def slider_val():
     return f"Slider valsss: {input.val()}"
 
-
+#rsconnect add --account ggfyumyum --name ggfyumyum --token 7786280A193F55618C6EE283771D22A9 --secret uapcUJM9pCePsljUVYZX1zgTaSZKX7koVsSxSSpm
 print('just testing!')
--- driver_code.py
+++ driver_code.py
@@ -12,42 +12,43 @@ from eq5d_decrement_processing import decrement_processing
 print('RUNNING')
 
 raw_data = pd.read_csv('fake_data.csv')
-dec = pd.read_excel('VS_decrements.xlsx')
+decrement_table = pd.read_excel('VS_decrements.xlsx')
 
+#if there is a valueset available, use it. Otherwise, use the decrement table to generate a fresh one.
 if os.path.exists('valueset_data.csv'):
     value_set = pd.read_csv('valueset_data.csv')
 else:
-    value_set = decrement_processing(dec).generate_value_set()
+    value_set = decrement_processing(decrement_table).generate_value_set()
     decrement_processing.export_value_set(value_set)
 
-#run the data through the validator
+#run the raw data through the validator
 data = Validator(raw_data).data
-print('data before',data)
 #append the utility scores and calculation to the original data
-data = eq5dvalue(data, value_set,'NewZealand')
-data = data.calculate_util()
-print('the data after',data)
+data = eq5dvalue(data, value_set,'NewZealand').calculate_util()
 
-
-#create a dictionary with each group having its own dataframe, specify which column the group label is
+#create a dictionary with each group having its own dataframe, store it in siloed_data
 group_col = 'TIME_INTERVAL'
 group1 = 'Preop'
 group2 = 'Postop'
 siloed_data = Processor(data,group_col).siloed_data
+
+#******************************************* DATA ANALYSIS *****************************************************
+
+#create some new DFs which can be used for plotting or printed directly for analysis
 t10_index = Processor(data,group_col).top_frequency()
 ts_delta_binary = Processor(data,group_col).ts_binary()
 paretian = Processor(data,group_col).paretian(group1,group2)
+data_LSS = Processor(data,group_col).level_frequency_score()
 
-data = Processor(data,group_col).level_frequency_score()
-data.to_csv('data_output.csv')
-
-#Viz(Processor(data,group_col).hpg(paretian,group1,group2)).hpg()
-
+#print(paretian)
+#print(t10_index)
+#print(ts_delta_binary)
+#print(t10_index)
 
 #print a simple descripton for each group and a binary format description
-def show_desc(data):
-    #input = a dictionary of siloed data
-    for group_name, group_data in data.items():
+def show_desc(siloed_data):
+    #input = a dictionary
+    for group_name, group_data in siloed_data.items():
         simple = Processor(group_data).simple_desc()
         binary = Processor(group_data).binary_desc()
         print('*' * 100)
@@ -58,21 +59,22 @@ def show_desc(data):
 
 show_desc(siloed_data)
 
-res = Processor(data).get_percent()
-Viz(res).histogram()
+#*************************** PLOTTING FUNCTIONS **********************************************
 
-#create a hashmap with percentages instead of full datasets to be used in grouped histogram
+#histogram of the dimension scores from the whole dataset
+as_pct = Processor(data).get_percent()
+Viz(as_pct).histogram()
+
+#side-by-side histograms comparing groups
 grouped_pct = {}
 for key, item in siloed_data.items():
     grouped_pct[key] = Processor(item).get_percent()
-
 Viz(grouped_pct).histogram_by_group()
 
+#health profile grid, requires the paretian dataframe
+Viz(Processor(data,group_col).hpg(paretian,group1,group2)).hpg()
 
-#Viz(ts_delta_binary).ts()
-#print(paretian)
-#print(t10_index)
-#print(ts_delta_binary)
-#print(t10_index)
+#time series of the binary score change
+Viz(ts_delta_binary).ts()
 
 print('done')
