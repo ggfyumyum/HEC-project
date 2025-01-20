ee28c2f40751671407160ba89cb300b277d8d72a
logan n <thegangreneforums@Gmail.com>
2025-01-21 02:32:40 +1300

    added docstrings and cleanup

--- eq5d_profile.py
+++ eq5d_profile.py
@@ -1,36 +1,68 @@
 import pandas as pd
 class Eq5dvalue:
-#This class will have some methods which deal with calculating utility
-#THe input will be some data containing eq5d profiles, the country label, and the value set
-#the output will be a df, which contains the util values mapped to the original UIDs.
+    """
+    This class calculates utility values for EQ-5D profiles based on a given value set and country.
 
+    Attributes:
+    value_set (pd.DataFrame): The value set containing utility weights.
+    data (pd.DataFrame): The input data containing EQ-5D profiles.
+    country (str): The country label for which the utility values are calculated.
+    """
     def __init__(self, data, valueset ,country='NewZealand'):
+        """
+        Initializes the Eq5dvalue class with the given data, value set, and country.
+        
+        Args:
+            data (pd.DataFrame): The input data containing EQ-5D profiles.
+            valueset (pd.DataFrame): The value set containing utility weights.
+            country (str): The country label for which the utility values are calculated. Default is 'NewZealand'.
+        Raises:
+            KeyError: If 'INDEX' is not found in the value set columns.
+        """
         self.value_set = valueset
         if 'INDEX' in self.value_set.columns:
             self.value_set.set_index('INDEX',inplace=True)
-
+        else:
+            raise KeyError("The value set must contain an 'INDEX' column.")
+    
         self.data = data
         self.country = country
-        self.utils = None
-
 
     def calculate_util(self):
-        #this function will calculate the utility values for each patient, based on the weights and the profile.
-        #return a new df which has the util value appended to the original data.
-        country = self.country
-        val = self.value_set
+        """
+        Calculates the utility values for each patient based on the weights and the profile.
+        
+        Returns:
+            pd.DataFrame: A new DataFrame with the utility values appended to the original data.
+        Raises:
+            KeyError: If 'INDEXPROFILE' is not found in the data columns.
+            KeyError: If the 'country' is not found in the value set columns.
+        """
+
         df = self.data
+
+        if 'INDEXPROFILE' not in df.columns:
+            raise KeyError("The data must contain an 'INDEXPROFILE' column.")
+        if self.country not in self.value_set.columns:
+            raise KeyError(f"The value set must contain a column for the country '{self.country}'.")
     
         #append the utility values to the original data
         self.util_values = []
         for index,row in df.iterrows():
             key= int(row['INDEXPROFILE'])
-            self.util_values.append(val.loc[key,country])
+            self.util_values.append(self.value_set.loc[key,self.country])
         df['UTILITY'] = self.util_values
 
         def create_util_ranking(utility_list):
-            #rank the utility scores from 1 to n for a given list of utility scores
-            #store the result in a hashmap with the key as index, rank as value
+            """
+            Ranks the utility scores from 1 to n for a given list of utility scores.
+            
+            Args:
+                utility_list (list): A list of utility scores.
+            
+            Returns:
+                dict: A dictionary with the index profile as the key and the rank as the value.
+            """
             df.sort_values(by='UTILITY',ascending=False,inplace=True)
             df.reset_index(inplace=True)
             result = {row['INDEXPROFILE']:index+1 for index, row in df.iterrows()}
@@ -44,8 +76,8 @@ class Eq5dvalue:
             ranked_util.append(util_to_rank[key])
         df['RANKED_UTILITY'] = ranked_util
 
-        #also create a new rank which is the index rank out of the TOTAL list of utilities for given country
-        total_ranking_df = val[country].reset_index().rename(columns={country: 'UTILITY'})
+        #also creates rank out of the TOTAL list of utilities for given country (used for HPG creation)
+        total_ranking_df = self.value_set[self.country].reset_index().rename(columns={self.country: 'UTILITY'})
         total_ranking_df.sort_values(by='UTILITY', ascending=False, inplace=True)
         total_ranking_df.reset_index(drop=True, inplace=True)
 
@@ -57,9 +89,8 @@ class Eq5dvalue:
             total_ranked_util.append(total_util_ranking[key])
         df['TOTAL_RANKED_UTILITY'] = total_ranked_util
 
-
-        self.utils = df
-        return self.utils
+        self.data = df
+        return df
 
 
 
